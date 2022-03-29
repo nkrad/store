52c0cc4e09c1b439a65455a76306fbbd4839fb78
nkrad <github@nkrad.ca>
2022-03-29 17:43:26 -0500

    added styling on home page

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,9 +1,12 @@
 class ProductsController < InheritedResources::Base
-
   private
 
-    def product_params
-      params.require(:product).permit(:name, :designer, :size, :description, :price)
-    end
+  def product_params
+    params.require(:product).permit(:name, :designer, :size, :description, :price)
+  end
 
+  def search
+    wildcard_search = "%#{params[:keywords]}%"
+    @products = Product.where("name LIKE ?", wildcard_search)
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -10,4 +10,12 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "articles#index"
+
+  resources :products, only: %i[index show] do
+    collection do
+      get "search"
+    end
+  end
+
+
 end
