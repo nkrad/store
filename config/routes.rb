f3730e677bce85d0fa703d8e846998a3c3f84316
nkrad <github@nkrad.ca>
2022-03-29 12:32:46 -0500

    generated home controller and updated route to point to that controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,10 @@
 Rails.application.routes.draw do
+  get "home/home"
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+
+  # sets the root path to the home controller
+  root to: 'home#home'
+
 
   # Defines the root path route ("/")
   # root "articles#index"
