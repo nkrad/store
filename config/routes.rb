e44bc155c522cfb5d37e395151ff5bd52c2cbfd5
nkrad <github@nkrad.ca>
2022-04-02 18:53:05 -0500

    added routes for cart

--- config/routes.rb
+++ config/routes.rb
@@ -2,15 +2,20 @@ Rails.application.routes.draw do
   devise_for :admin_users, ActiveAdmin::Devise.config
   ActiveAdmin.routes(self)
   resources :products
-  get "home/home"
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # sets the root path to the home controller
+  get "home/home"
   root to: "home#home"
 
-  # Defines the root path route ("/")
-  # root "articles#index"
+  resources :cart, only: %i[create destroy]
 
+  # change to post
+  # get "products/add_to_cart/:id", to: "products#add_to_cart", as: "add_to_cart"
+  # post "products/add_to_cart/:id", to: "products#add_to_cart", as: "add_to_cart"
+
+  # delete "products/remove_from_cart/:id", to: "products#remove_from_cart", as: "remove_from_cart"
+
+  # taken from intro project, doesnt work
   resources :products, only: %i[home show] do
     collection do
       get "search"
