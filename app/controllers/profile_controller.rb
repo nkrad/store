e0409847a9e9d5decc5b22743cc3735dacc22c40
nkrad <github@nkrad.ca>
2022-04-12 16:05:48 -0500

    created profile controller

---
+++ app/controllers/profile_controller.rb
@@ -0,0 +1,3 @@
+class ProfileController < ApplicationController
+  def edit; end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  get 'profile/edit'
   devise_for :users
   get "category/index"
   devise_for :admin_users, ActiveAdmin::Devise.config
