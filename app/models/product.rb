2869fafe0caae3b55aa75523d50af99df3c3398c
nkrad <github@nkrad.ca>
2022-04-17 15:30:55 -0500

    added associations

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,5 +1,6 @@
 class Product < ApplicationRecord
   belongs_to :category
+  belongs_to :order_detail
 
   paginates_per 8
 
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_04_12_183633) do
+ActiveRecord::Schema[7.0].define(version: 2022_04_17_201319) do
   create_table "active_admin_comments", force: :cascade do |t|
     t.string "namespace"
     t.text "body"
@@ -79,6 +79,24 @@ ActiveRecord::Schema[7.0].define(version: 2022_04_12_183633) do
 
   create_table "categories", force: :cascade do |t|
     t.string "category"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "order_details", force: :cascade do |t|
+    t.integer "order_id"
+    t.integer "user_id"
+    t.integer "product_id"
+    t.integer "quantity"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "orders", force: :cascade do |t|
+    t.integer "user_id"
+    t.float "subtotal"
+    t.float "tax"
+    t.float "grand_total"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
